caa25f2abd9ac86a5136e90901e3f6ae195a594c
suppakornw-tech <suppakorn.w@ku.th>
2026-07-30 21:04:29 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -1,6 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// create by suppakorn wannaprabha 
+//           ศุภกร วรรณประภา
+//           6821601496
+
+/**
+     * BoundedStack - ADT แทน Stack ที่ถูกกำหนดขอบเขตชัดเจน
+     * 
+     * ค่านามธรรม (A): ลำดับของค่าที่เรียงจากค่าที่เข้าก่อนไปค่าที่เข้าทีหลัง เช่น [1,2] หมายถึง 2 อยู่บนสุด
+     * 
+     * ตัวอย่างการใช้งาน: 
+     *      BoundedStack s = new BoundedStack(3);
+     *      s.push(1);
+     *      s.push(2);
+     *      System.out.println(s.peek());   // 2
+     *      s.pop();
+     *      System.out.println(s.peek());   // 1
+     */
+
 public class BoundedStack {
     private final List<String> data;
     private final int capacity;
@@ -22,6 +40,14 @@ public class BoundedStack {
         assert data.size() >= 0 && data.size() <= capacity : "data is out of range";
     }
 
+    //==== Creators ====
+    // create BoundedStack the capacity will be pass from the user  
+    /**
+     * @param capacity the maximum data that can collect in this stack
+     * @post Stack will be create with the empty data. the size will be 0
+     * @throws IllegalArgumentException if capacity <= 0
+     * 
+     */
     public BoundedStack(int capacity) {
         if (capacity <= 0) throw new IllegalArgumentException();
 
@@ -30,6 +56,15 @@ public class BoundedStack {
         this.capacity = capacity;
         checkRep();
     }
+
+    //==== Mutators ====
+    // add the data to the top stack 
+    /**
+     * @param x is the data that we add to the stack
+     * @pre size will be less than the capacity
+     * @post size + 1
+     * @throws IllegalArgumentException if size = capacity
+     */
 
     public boolean push(String name) {
         if (name == null || name == "") throw new IllegalArgumentException();
@@ -41,6 +76,14 @@ public class BoundedStack {
         return true;
     }
 
+    // remove the lastest data
+    /**
+     * @pre stack cannot be empty
+     * @post size will be -1 
+     * @return the data that get remove
+     * @throws IllegalArgumentException if the stack is all ready empty
+     */
+
     public boolean pop() {
         if (top <= 0) return false;
 
@@ -50,6 +93,12 @@ public class BoundedStack {
         return true;
     }
 
+    //==== Obsevers ====
+    //  return data that on the top stack right now
+    /**
+     * @return top data of this stack
+     * @throws IllegalArgumentException if the stack is empty
+     */
     public String peek() {
         if (top <= 0) {
             throw new IllegalArgumentException();
@@ -58,17 +107,40 @@ public class BoundedStack {
         return data.get(top - 1);
     }
 
+    // return position of the lastest data 
+    /**
+     * @return position of the lastest data
+     */
+
     public int size() {
         return top;
     }
+
+    // return capacity
+    /**
+     * @return capacity
+     */
 
     public int capacity() {
         return capacity;
     }
 
+    // return ArrayList that collect all the same data
+    /**
+     * @return ArrayList that collect all the same data
+     */
+
     public List<String> copy() {
         return new ArrayList<>(data);
     }
+
+    // ==== Producers ====
+    // return BoundedStack that completely reverse
+    /**
+     * 
+     * @post be the same size() , if original is ["apple", "pen"] reversed wil be ["pen", "apple"]
+     * @return reverse BoundedStack
+     */
 
     public BoundedStack reverse() {
         BoundedStack reversed = new BoundedStack(capacity);
